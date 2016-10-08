281af36ffbb74a67d0678a3ef175d685776dec25
Edward Faulkner <ef@alum.mit.edu>
2016-10-08 18:46:27 -0400

    creating scenario

---
+++ app/controllers/application.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  items: Array.from(new Array(500).keys()),
+
+  actions: {
+    toggle: function() {
+      var start = performance.now();
+
+      this.set('delay', '');
+      Ember.run.schedule('afterRender', this, function() {
+        this.set('delay', performance.now() - start);
+      });
+    },
+
+    outerToggle: function() {
+      var start = performance.now();
+
+      this.set('outerDelay', '');
+      this.set('showOuter', !this.get('showOuter'));
+      Ember.run.schedule('afterRender', this, function() {
+        this.set('outerDelay', performance.now() - start);
+      });
+    }
+
+  }
+});
